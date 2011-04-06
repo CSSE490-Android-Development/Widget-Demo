b52bcf743021d2927f8e1b13e4007df7c5888594
Jimmy Theis <jimmy@jetheis.com>
2011-04-05 21:02:45 -0400

    Add hard-coded brightness setting
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- Flashlight/src/com/rosehulman/android/flashlight/Flashlight.java
+++ Flashlight/src/com/rosehulman/android/flashlight/Flashlight.java
@@ -2,12 +2,24 @@ package com.rosehulman.android.flashlight;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Flashlight extends Activity {
+    
+    private boolean mFullBright = true;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setBrightness();
+    }
+    
+    private void setBrightness() {
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.screenBrightness = (float) (mFullBright ? 1.0 : -1.0); // or WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL : WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE 
+                                                                   // on 2.2 (API 8) or above
+        getWindow().setAttributes(lp);
     }
 }
