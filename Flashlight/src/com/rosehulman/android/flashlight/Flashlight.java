4a9f6982984effba39d690879fa9faa3d59b4318
Jimmy Theis <jimmy@jetheis.com>
2011-04-05 23:07:33 -0400

    Finish impplementing preference changing
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- Flashlight/src/com/rosehulman/android/flashlight/Flashlight.java
+++ Flashlight/src/com/rosehulman/android/flashlight/Flashlight.java
@@ -1,33 +1,66 @@
 package com.rosehulman.android.flashlight;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 
 public class Flashlight extends Activity {
     
     private SharedPreferences mPrefs;
     private boolean mFullBright;
+    private String mColorKey;
+    
+    private View mLightView;
+    
+    private Map<String, Integer> mColors;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        mPrefs = getPreferences(MODE_PRIVATE);
-        mFullBright = mPrefs.getBoolean(getString(R.string.pref_key_fullbright), true);
-        setBrightness();
+        
+        mLightView = findViewById(R.id.flashlight_light);
+        
+        mColors = new HashMap<String, Integer>();
+        mColors.put(getString(R.string.pref_key_color_white), R.color.white);
+        mColors.put(getString(R.string.pref_key_color_red), R.color.red);
+        mColors.put(getString(R.string.pref_key_color_green), R.color.green);
+        mColors.put(getString(R.string.pref_key_color_blue), R.color.blue);
+        
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
     }
     
-    private void setBrightness() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateBrightness();
+        updateColor();
+    }
+    
+    private void updateBrightness() {
+        mFullBright = mPrefs.getBoolean(getString(R.string.pref_key_fullbright), true);
+        
         WindowManager.LayoutParams lp = getWindow().getAttributes();
         lp.screenBrightness = (float) (mFullBright ? 1.0 : -1.0); // or WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL : WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE 
                                                                    // on 2.2 (API 8) or above
         getWindow().setAttributes(lp);
+    }
+    
+    private void updateColor() {
+        mColorKey = mPrefs.getString(getString(R.string.pref_key_color), getString(R.string.pref_key_color_white));
+        mLightView.setBackgroundResource(mColors.get(mColorKey));
     }
     
     @Override
